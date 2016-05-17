30f8bc78178058367694d08923841bcfef402968
moller-g-peter <moller.g.peter@gmail.com>
2016-05-17 21:27:06 +0200

    cheating with zoom

--- js/js_template.js
+++ js/js_template.js
@@ -1,11 +1,14 @@
 
+
+
+
 var mainVideo = document.getElementById("mainVideo");
 var staticVideo = document.getElementById("staticVideo");
 
 
 $('.greenButton').hide();
-$('#staticVideo').prop('muted', 'false');
-
+$('#staticVideo').prop('muted', 'true');
+// $('#mainVideo').prop('muted', 'false');
 
 // STATIC VIDEO
 function playStaticVideo() {
@@ -17,39 +20,43 @@ function playStaticVideo() {
 // MAIN VIDEO FUNCTIONS
 function playMainVideo() {
   mainVideo.play();
-  mainVideo.loop = true;
+  mainVideo.loop = false;
 }
 
 
 
-$('.blackButton').click(function(){
+$('.blackButton').click(function(ev){
 	$('#staticVideo').hide();
 	$('.blackButton').hide();
 	$('.greenButton').show();
-	playMainVideo();
-
+  // $('#mainVideo').prop('muted', 'false');
+  playMainVideo();
+  // console.log("playMainVideo :", playMainVideo);
 	// $("#mainVideo").prop('muted', !$("#mainVideo").prop('muted'));
 
-	if ($('#mainVideo').prop('muted')){
-		$(this).prop('muted', 'false');
-	} else {
-		$(this).prop('muted', 'true');
-	}
+	// if ($('#mainVideo').prop('muted', 'true')){
+	// 	$(this).prop('muted', 'false');
+	// }
+ //  else {
+	// 	$(this).prop('muted', 'true');
+	// }
 
 });
 
 
 
 
-$('.greenButton').click(function(){
+$('.greenButton').click(function(evII){
 	// $('#mainVideo').hide();
 	$('#staticVideo').show();
 	$('.blackButton').show();
 	$('.greenButton').hide();
-
-	// if ($('#mainVideo').prop('muted', 'false')){
+  $('#mainVideo').prop('muted', 'true');
+  console.log("evII :", evII);
+	// if (!$('#mainVideo').prop('muted')){
 	// 	$(this).prop('muted', 'true');
-	// } else {
+	// }
+ //  else {
 	// 	$(this).prop('muted', 'false');
 	// }
 
